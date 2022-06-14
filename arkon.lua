aa6b38ce12f71cf46cce518fa3ae659c847a3956
arkonsource <107515750+arkonsource@users.noreply.github.com>
2022-06-15 02:10:10 +0300

    Update arkon.lua

--- arkon.lua
+++ arkon.lua
@@ -11955,8 +11955,8 @@ LuaTele.sendText(msg_chat_id,msg_id,Reply_Status(Info_User,'- تم ارسال ر
 end
 end
 end 
-end
-end
+end --UserBot
+end -- File_Bot_Run
 
 
 function CallBackLua(data) --- هذا الكالباك بي الابديت
