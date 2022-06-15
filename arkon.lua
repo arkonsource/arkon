9b37a87cf92dc4426bd699cdb4e5313d58309392
arkonsource <107515750+arkonsource@users.noreply.github.com>
2022-06-15 13:50:16 +0300

    Update arkon.lua

--- arkon.lua
+++ arkon.lua
@@ -9915,10 +9915,17 @@ local rep = msg.id/2097152/0.5
 https.request("https://api.telegram.org/bot"..Token.."/sendphoto?chat_id="..msg_chat_id.."&caption="..URL.escape(t).."&photo="..m.."&reply_to_message_id="..rep.."&parse_mode=Markdown")
 end
 if text == 'السورس' or text == 'سورس' or text == 'يا سورس' or text == 'ياسورس' then
-keyboard = {} 
-keyboard.inline_keyboard = {{{text = '𝘢𝘳𝘬𝘰𝘯 𝘴𝘰𝘶𝘳𝘤𝘦 .', url = "https://t.me/ArkonSource"}}}
-https.request("https://api.telegram.org/bot"..Token.."/sendphoto?chat_id=" .. msg_chat_id .. "&photo=https://t.me/ArkonSource&caption=".. URL.escape("𝘸𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘵𝘩𝘦 𝘢𝘳𝘬𝘰𝘯 𝘴𝘰𝘶𝘳𝘤𝘦 .").."&reply_to_message_id="..(msg_id/2097152/0.5).."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-end
+  photo = "https://t.me/Picturarkon/2"
+  local T =[[
+    𝘸𝘦𝘭𝘤𝘰𝘮𝘦 𝘵𝘰 𝘵𝘩𝘦 𝘢𝘳𝘬𝘰𝘯 𝘴𝘰𝘶𝘳𝘤𝘦 .
+  ]]
+  keyboard = {} 
+  keyboard.inline_keyboard = {
+  {
+  {text = ' 𝘢𝘳𝘬𝘰𝘯 𝘴𝘰𝘶𝘳𝘤𝘦 . ', url = "https://t.me/ArkonSource"}}}
+  local msgg = msg_id/2097152/0.5
+  https.request("https://api.telegram.org/bot"..Token.."/sendphoto?chat_id=" .. msg_chat_id .. "&photo="..photo.."&caption=".. URL.escape(T).."&reply_to_message_id="..msgg.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))  
+
 elseif text == 'الاوامر' then
 if not msg.Addictive then
 return LuaTele.sendText(msg_chat_id,msg_id,'\n*-هاذا الامر يخص { '..Controller_Num(7)..' }* ',"md",true)  
